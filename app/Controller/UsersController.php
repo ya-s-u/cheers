e69fe082e363abae0446d0f56c8986dc3b31f3c5
後藤 誉昌 <yasu1003@gmail.com>
2014-10-19 05:05:05 +0900

    やっぱりFBログインやめる.....

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -2,7 +2,8 @@
 
 class UsersController extends AppController {
     public $name = 'Users';
-    public $components = array(
+    /*
+public $components = array(
 		'Auth' => array(
 	        'authenticate' => array(
 	            'Form' => array(
@@ -20,28 +21,26 @@ class UsersController extends AppController {
 	        'logoutRedirect' => array('controller' => 'posts', 'action' => 'index'),
 	    ),
 	);
+*/
 
     public function beforeFilter() {
         parent::beforeFilter();
         
-        $this->Auth->allow('login');
+        /*
+$this->Auth->allow('login');
 		$this->Auth->allow('fb_login');
 		$this->Auth->allow('signup');
 		$this->Auth->allow('fb_signup');
+*/
         
-		$auth_id = $this->Auth->user('id');
+		/*
+$auth_id = $this->Auth->user('id');
 		if($auth_id) {
 			$auth = $this->User->getUser($auth_id);
 			$this->set('auth',$auth);
 		}
+*/
 		
-		App::import('Vendor', 'facebook/src/facebook');
-        $this->facebook = new Facebook( array(
-	        'appId' => '782740005121656',
-	        'secret' => '98039e33232084062ad5d62fbefde9d5',
-	        'cookie' => true,
-	        'locale' => 'ja_JP'
-        ));
     }
 
     /* トップページ */
@@ -49,41 +48,6 @@ class UsersController extends AppController {
         $this->set('title_for_layout', 'ダッシュボード');
     }
     
-    /* FB登録 */
-    public function signup() {
-        $this->set('title_for_layout', '新規登録');
-        
-		$fb = $this->facebook->getUser();
-        if(empty($fb)){
-       	    $login_url = $this->facebook->getLoginUrl(array('scope' => 'email'));
-        	$this->redirect($login_url);
-        	
-			$me = $this->facebook->api('/me');
-			$this->set('f1',$me['name']);
-        }
-        
-		$me = $this->facebook->api('/me',
-	    	array(
-	    		'locale' => 'ja_JP',
-	    	)
-	    );
-		$this->set('me',$me);
-		
-		$this->User->set($Data);
-		$this->User->validates();
-		$this->User->create();
-		$this->data = array(
-			'facebook_user_id' => $me['id'],
-			'facebook_user_name' => $me['name'],
-			'facebook_user_mail' => $me['email'],
-			'created' => date("Y-m-d G:i:s"),
-			'modified' => date('Y-m-d H:i:s'),
-		);
-		$this->User->save($this->data);
-		
-		$this->redirect($this->referer());
-    }
-
     /* ログアウト */
     public function logout() {
         $this->set('title_for_layout', 'ログアウト');
